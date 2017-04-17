a2c9ec3d040eb95d35e7df6ac076a6386b1c709c
Trapenok Victor <levhav@yandex.ru>
2017-04-17 11:40:42 +1000

    add static files for GUI

--- ihservice/main/settings.py
+++ ihservice/main/settings.py
@@ -195,7 +195,7 @@ STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.FileSystemFinder',
   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Celery settings
 
