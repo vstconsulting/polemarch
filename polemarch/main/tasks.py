f8483838041eb50741e0780b78ac47b6bcb6e140
cepreu <cepreu.mail@gmail.com>
2017-04-24 11:22:52 +1000

    WIP at decorator for task

--- polemarch/main/tasks.py
+++ polemarch/main/tasks.py
@@ -1,30 +1,31 @@
+# pylint: disable=broad-except
 import random
-
 import logging
-
 from polemarch.celery_app import app
 
 logger = logging.getLogger("polemarch")
 
+def task(original_class, *args, **kwargs):
+    pass
 
 class TestTask(object):
-    def __init__(self, job, a, b):
-        logger.info("making sum of " + str(a) + " and " + str(b))
+    def __init__(self, job, arg1, arg2):
+        logger.info("making sum of " + str(arg1) + " and " + str(arg2))
         try:
             if random.randint(1, 10) > 5:
                 raise Exception("Unfortunately we can't proceed with this "
                                 "random number")
-            self.a = a
-            self.b = b
+            self.arg1 = arg1
+            self.arg2 = arg2
             super(TestTask, self).__init__()
         except Exception as e:
             job.retry(exc=e)
 
     def run(self):
-        return self.a + self.b
+        return self.arg1 + self.arg2
 
 
 @app.task(ignore_result=True, default_retry_delay=10,
-      max_retries=10, bind=True)
-def test(self, a, b):
-    return TestTask(self, a, b).run()
+          max_retries=10, bind=True)
+def test(self, arg1, arg2):
+    return TestTask(self, arg1, arg2).run()
