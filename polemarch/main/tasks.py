5dede31527d23c564b9f4f1daa2ee2e346519076
Sergey Klyuykov <onegreyonewhite@mail.ru>
2017-04-24 12:53:28 +1000

    done: #140
    * Add main.utils.task decorator for tasks
    * Add main.utils.BaseTask for default tasks

--- polemarch/main/tasks.py
+++ polemarch/main/tasks.py
@@ -1,23 +1,23 @@
 # pylint: disable=broad-except
-import random
 import logging
-from polemarch.celery_app import app
+from ..celery_app import app
+from .utils import task, BaseTask
 
 logger = logging.getLogger("polemarch")
 
-def task(original_class, *args, **kwargs):
-    pass
 
-class TestTask(object):
+@task(app, ignore_result=True, default_retry_delay=10,
+      max_retries=10, bind=True)
+class TestTask(BaseTask):
     def __init__(self, job, arg1, arg2):
+        super(self.__class__, self).__init__(job, arg1, arg2)
         logger.info("making sum of " + str(arg1) + " and " + str(arg2))
         try:
-            if random.randint(1, 10) > 5:
+            if arg1 == 1:
                 raise Exception("Unfortunately we can't proceed with this "
-                                "random number")
+                                     "random number")
             self.arg1 = arg1
             self.arg2 = arg2
-            super(TestTask, self).__init__()
         except Exception as e:
             job.retry(exc=e)
 
@@ -25,7 +25,17 @@ class TestTask(object):
         return self.arg1 + self.arg2
 
 
-@app.task(ignore_result=True, default_retry_delay=10,
-          max_retries=10, bind=True)
-def test(self, arg1, arg2):
-    return TestTask(self, arg1, arg2).run()
+@task(app, ignore_result=True, default_retry_delay=10,
+      max_retries=10, bind=True)
+class TestTask2(BaseTask):
+    def __init__(self, job, arg1, arg2):
+        super(self.__class__, self).__init__(job, arg1, arg2)
+        logger.info("making sum of " + str(arg1) + " and " + str(arg2))
+        try:
+            self.arg1 = arg1
+            self.arg2 = arg2
+        except Exception as e:
+            job.retry(exc=e)
+
+    def run(self):
+        return self.arg1 * self.arg2
