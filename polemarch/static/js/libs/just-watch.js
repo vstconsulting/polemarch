4f82644349b945c9a9fbf2ad24c3579bd0c7ce93
Trapenok Victor <levhav@yandex.ru>
2017-07-12 16:04:23 +1000

    #165 fix

--- polemarch/static/js/libs/just-watch.js
+++ polemarch/static/js/libs/just-watch.js
@@ -492,7 +492,7 @@ Object.defineProperty(Object.prototype, "justAttr", {
   , value: function(prop, attrName, callBack, customData){ return justReactive.setValue.apply(this, [{type:'attr', prop:prop, callBack:callBack, attrName:attrName, customData:customData}])}
 });
 
-// Проставляет значение как css class
+//  Добавление точки отслеживания 
 Object.defineProperty(Object.prototype, "justWatch", {
     enumerable: false
   , configurable: true
--- polemarch/static/js/pmHistory.js
+++ polemarch/static/js/pmHistory.js
@@ -33,8 +33,8 @@ pmHistory.search = function(project_id, query)
     }
 
     return spajs.open({ menuId:'project/' + project_id +"/" + this.model.name+"/search/"+encodeURIComponent(trim(query)), reopen:true});
-}
- 
+} 
+
 pmHistory.showListInProjects = function(holder, menuInfo, data)
 {
     var thisObj = this;
@@ -46,7 +46,7 @@ pmHistory.showListInProjects = function(holder, menuInfo, data)
     }
     var project_id = data.reg[1];
 
-    return $.when(this.searchItems(project_id, 'project'), pmProjects.loadItem(project_id)).done(function()
+    return $.when(this.sendSearchQuery({project:project_id}, limit, offset), pmProjects.loadItem(project_id)).done(function()
     {
         $(holder).html(spajs.just.render(thisObj.model.name+'_listInProjects', {query:"", project_id:project_id}))
     }).fail(function()
@@ -81,4 +81,178 @@ pmHistory.showItemInProjects = function(holder, menuInfo, data)
     {
         $.notify("", "error");
     })
+}
+
+
+/**
+ * Обновляет поле модел this.model.items[item_id] и ложит туда пользователя
+ */
+pmHistory.loadItem = function(item_id)
+{
+    var def = new $.Deferred();
+    var thisObj = this;
+     
+    jQuery.ajax({
+        url: "/api/v1/"+this.model.name+"/"+item_id+"/",
+        type: "GET",
+        contentType:'application/json',
+        data: "",
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        },
+        success: function(data)
+        {
+            console.log("loadUser", data)
+            thisObj.model.items[item_id] = data
+            $.when(pmProjects.loadItem(data.project)).done(function(){
+                def.resolve()
+            }).fail(function(){
+                def.reject()
+            }) 
+        },
+        error:function(e)
+        {
+            console.log(e)
+            polemarch.showErrors(e)
+            def.reject()
+        }
+    });
+     
+    return def.promise();
+}
+
+pmHistory.sendSearchQuery = function(query, limit, offset)
+{
+    if(!limit)
+    {
+        limit = 999;
+    }
+
+    if(!offset)
+    {
+        offset = 0;
+    }
+
+    var q = [];
+    for(var i in query)
+    {
+        q.push(encodeURIComponent(i)+"="+encodeURIComponent(query[i])) 
+    }
+
+    var def = new $.Deferred();
+    var thisObj = this;
+    jQuery.ajax({
+        url: "/api/v1/"+this.model.name+"/?"+q.join('&'),
+        type: "GET",
+        contentType:'application/json',
+        data: "limit="+encodeURIComponent(limit)+"&offset="+encodeURIComponent(offset),
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        },
+        success: function(data)
+        {
+            console.log("update Items", data)
+            thisObj.model.itemslist = data
+            //thisObj.model.items = {}
+
+            var projects = [];
+            for(var i in data.results)
+            {
+                var val = data.results[i]
+                thisObj.model.items[val.id] = val
+                
+                if(!pmProjects.model.items[val.project] && projects.indexOf(val.project) == -1)
+                {
+                    projects.push(val.project)    
+                }
+            }
+            
+            $.when(pmProjects.sendSearchQuery({id:projects.join(',')})).done(function(){
+                def.resolve()
+            }).fail(function(){
+                def.reject()
+            }) 
+        },
+        error:function(e)
+        {
+            console.log(e)
+            polemarch.showErrors(e)
+            def.reject()
+        }
+    });
+     
+    return def.promise();
+}
+
+/**
+ * Обновляет поле модел this.model.itemslist и ложит туда список пользователей
+ * Обновляет поле модел this.model.items и ложит туда список инфу о пользователях по их id
+ */
+pmHistory.loadItems = function(limit, offset)
+{
+    if(!limit)
+    {
+        limit = 30;
+    }
+
+    if(!offset)
+    {
+        offset = 0;
+    }
+
+    var def = new $.Deferred();
+    var thisObj = this; 
+    jQuery.ajax({
+        url: "/api/v1/"+this.model.name+"/",
+        type: "GET",
+        contentType:'application/json',
+        data: "limit="+encodeURIComponent(limit)+"&offset="+encodeURIComponent(offset),
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        },
+        success: function(data)
+        {
+            console.log("update Items", data)
+            thisObj.model.itemslist = data
+            thisObj.model.itemslist.limit = limit
+            thisObj.model.itemslist.offset = offset
+            //thisObj.model.items = {}
+
+            var projects = [];
+            for(var i in data.results)
+            {
+                var val = data.results[i]
+                thisObj.model.items.justWatch(val.id);
+                thisObj.model.items[val.id] = mergeDeep(thisObj.model.items[val.id], val)
+                
+                if(!pmProjects.model.items[val.project] && projects.indexOf(val.project) == -1)
+                {
+                    projects.push(val.project)    
+                }
+            }
+            
+            $.when(pmProjects.sendSearchQuery({id:projects.join(',')})).done(function(){
+                def.resolve()
+            }).fail(function(){
+                def.reject()
+            }) 
+        },
+        error:function(e)
+        {
+            console.log(e)
+            polemarch.showErrors(e)
+            def.reject()
+        }
+    });
+     
+    return def.promise();
 }
