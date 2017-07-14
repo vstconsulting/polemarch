0feb212d581fed7cc807c67d73267ff17c17855b
Sergey Klyuykov <onegreyonewhite@mail.ru>
2017-07-14 18:29:44 +1000

    After release fixes * 'rest_framework.renderers.AdminRenderer' changet to 'rest_framework.renderers.BrowsableAPIRenderer' * fix tags test (rpm release)

--- polemarch/main/settings.py
+++ polemarch/main/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.AdminRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.AdminRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "polemarch.api.permissions.ModelPermission",
