187e0ba9fc72587abaca74211c4b1c3c7424e861
Trapenok Victor <levhav@yandex.ru>
2017-08-11 15:30:14 +1000

    export and import templates

--- polemarch/static/js/pmTemplates.js
+++ polemarch/static/js/pmTemplates.js
@@ -1,6 +1,6 @@
 
 var pmTemplates = new pmItems()
-pmTemplates.model.name = "templates" 
+pmTemplates.model.name = "templates"
 
 
 // Поддерживаемые kind /api/v1/templates/supported-kinds/
@@ -8,12 +8,12 @@ pmTemplates.model.kind = "Task,Module"
 
 // Содержит соответсвия разных kind к объектами с ними работающими.
 pmTemplates.model.kindObjects = {}
- 
+
 pmTemplates.showSearchResults = function(holder, menuInfo, data)
 {
     var thisObj = this;
-    var query = decodeURIComponent(data.reg[1]) 
-    
+    var query = decodeURIComponent(data.reg[1])
+
     return $.when(this.sendSearchQuery({kind:thisObj.model.kind, name:query})).done(function()
     {
         $(holder).html(spajs.just.render(thisObj.model.name+'_list', {query:query}))
@@ -35,15 +35,138 @@ pmTemplates.showList = function(holder, menuInfo, data)
 
     return $.when(this.sendSearchQuery({kind:thisObj.model.kind}, limit, offset)).done(function()
     {
-        $(holder).html(spajs.just.render(thisObj.model.name+'_list', {query:""})) 
+        $(holder).html(spajs.just.render(thisObj.model.name+'_list', {query:""}))
     }).fail(function()
     {
         $.notify("", "error");
     }).promise()
 }
 
-   
-pmTemplates.exportToFile = function()
+pmTemplates.importFromFile = function(files_event)
+{  
+    var def = new $.Deferred(); 
+    this.model.files = files_event
+    
+    for(var i=0; i<files_event.target.files.length; i++)
+    {
+        var reader = new FileReader();
+        reader.onload = (function(index_in_files_array)
+        {
+            return function(e)
+            {
+                console.log(e)
+                var bulkdata = []
+                var filedata = JSON.parse(e.target.result)
+                for(var i in filedata.data)
+                {
+                    var val = filedata.data[i]
+                    val.type = "add"
+                    bulkdata.push(val)
+                }
+                console.log(bulkdata)
+                 
+                $.ajax({
+                    url: "/api/v1/_bulk/",
+                    type: "POST",
+                    contentType:'application/json',
+                    data:JSON.stringify(bulkdata),
+                    beforeSend: function(xhr, settings) {
+                        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                            // Only send the token to relative URLs i.e. locally.
+                            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                        }
+                    },
+                    success: function(data)
+                    { 
+                        def.resolve();
+                        spajs.openURL(window.location.href);
+                    },
+                    error:function(e)
+                    {
+                        console.warn(e)
+                        polemarch.showErrors(e)
+                        def.reject();
+                    }
+                });
+            };
+        })(i);
+        reader.readAsText(files_event.target.files[i]); 
+        
+        // Нет поддержки загрузки более одного файла за раз.
+        break;
+    }
+    
+    return def.promise();
+}
+
+pmTemplates.exportToFile = function(item_ids)
 {
-   // $.when()
+    var def = new $.Deferred();
+    if(!item_ids)
+    {
+        $.notify("No data for export", "error");
+        def.reject();
+        return def.promise();
+    }
+
+    var data = {
+        "filter": {
+            "id__in": item_ids,
+        },
+    }
+
+    $.ajax({
+        url: "/api/v1/templates/filter/?detail=1",
+        type: "POST",
+        contentType:'application/json',
+        data:JSON.stringify(data),
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        },
+        success: function(data)
+        {
+            var filedata = []
+            for(var i in data.results)
+            {
+                var val = data.results[i]
+                delete val['id'];
+                delete val['url'];
+
+                filedata.push({ 
+                    item: "template",
+                    data: val
+                })
+            }
+            
+            var fileInfo = {
+                data:filedata,
+                count:filedata.length
+            }
+            
+            var textFileAsBlob = new Blob([JSON.stringify(fileInfo)], {
+              type: 'text/plain'
+            });
+
+            var newLink = document.createElement('a')
+            newLink.href = window.URL.createObjectURL(textFileAsBlob)
+            newLink.download = "pmTemplates-"+Date()+".json"
+            newLink.target = "_blanl"
+            var event = new MouseEvent("click");
+            newLink.dispatchEvent(event);
+
+
+            def.resolve();
+        },
+        error:function(e)
+        {
+            console.warn(e)
+            polemarch.showErrors(e)
+            def.reject();
+        }
+    });
+
+    return def.promise();
 }
