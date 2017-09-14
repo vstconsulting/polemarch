6cb0d0869a004b628342d840004a83859203cd98
cepreu <cepreu.mail@gmail.com>
2017-09-14 16:57:35 +1000

    environment variables in one place

--- polemarch/celery_app.py
+++ polemarch/celery_app.py
@@ -1,15 +1,8 @@
-import os
-
 from celery import Celery
 
+from .environment import prepare_environment
 
-# ansible specific environment variables
-os.environ.setdefault('ANSIBLE_HOST_KEY_CHECKING', 'False')
-os.environ.setdefault("ANSIBLE_FORCE_COLOR", "true")
-# django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'polemarch.main.settings')
-# run Celery
+prepare_environment()
 app = Celery('polemarch')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
