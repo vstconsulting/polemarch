4539c545bd0a23ef9542122f46d9fb0c970ca785
cepreu <cepreu.mail@gmail.com>
2017-09-14 16:37:18 +1000

    slightly prettier ansible environment variables

--- polemarch/celery_app.py
+++ polemarch/celery_app.py
@@ -3,10 +3,13 @@ import os
 from celery import Celery
 
 
+# ansible specific environment variables
 os.environ.setdefault('ANSIBLE_HOST_KEY_CHECKING', 'False')
+os.environ.setdefault("ANSIBLE_FORCE_COLOR", "true")
+# django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'polemarch.main.settings')
-os.environ.setdefault("ANSIBLE_FORCE_COLOR", "true")
+# run Celery
 app = Celery('polemarch')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
