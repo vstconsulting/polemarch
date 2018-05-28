fa4a6060de2b9e3c96ec0cdca7991c9ee1e3b6ce
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-05-28 19:16:34 +1000

    Merge branch 'developer' of /home/grey/vst_projects/polemarch with conflicts.

--- setup.py
+++ setup.py
@@ -1,52 +1,9 @@
 import os
 import sys
-
-try:
-    from sphinx.setup_command import BuildDoc
-    import sphinx
-    has_sphinx = True
-except ImportError:
-    has_sphinx = False
-
-from setuptools import find_packages, setup
-from setuptools.command.install import install
-from setuptools.command.build_ext import build_ext as _build_ext
-from setuptools.command.sdist import sdist as _sdist
-from setuptools.extension import Extension
-
-try:
-    from Cython.Distutils import build_ext as _build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    has_cython = False
-else:
-    has_cython = True
-
+from vstutils.compile import make_setup, load_requirements
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-RMF = os.path.join(os.path.dirname(__file__), 'README.rst')
-with open(RMF) as readme:
-    README = readme.read()
-
-
-def load_requirements(file_name):
-    with open(os.path.join(os.path.dirname(__file__), file_name)) as req_file:
-        return req_file.read().strip().split('\n')
-
-
-REQUIRES = load_requirements('requirements.txt')
-REQUIRES += load_requirements('requirements-doc.txt')
-REQUIRES_git = load_requirements('requirements-git.txt')
-
-
-if 'compile' in sys.argv:
-    use_cython = True
-    ext = ".py"
-else:
-    use_cython = False
-    ext = '.c'
 
 ext_list = [
     "polemarch.api.v1.filters",
@@ -66,9 +23,7 @@ ext_list = [
     "polemarch.main.models.utils",
     "polemarch.main.models.users",
     "polemarch.main.models.vars",
-    # "polemarch.main.tasks.tasks",
     'polemarch.main.settings',
-    #'polemarch.main.utils',
     'polemarch.main.hooks.base',
     'polemarch.main.hooks.http',
     'polemarch.main.hooks.script',
@@ -82,68 +37,27 @@ ext_list = [
 
 ]
 
-if not os.path.exists(ext_list[0].replace(".", "/")+ext):
-    ext = ".py" if ext == ".c" else ".c"
-
-extensions_dict = dict((
-    (exten, [exten.replace(".", "/")+ext]) for exten in ext_list
-))
-
 if 'develop' in sys.argv:
     ext_modules = []
-else:
-    ext_modules = list(Extension(m, f) for m, f in extensions_dict.items())
-    ext_count = len(ext_modules)
-    nthreads = ext_count if ext_count < 10 else 10
+    ext_list = []
 
-    if use_cython and has_cython:
-        ext_modules = cythonize(
-            ext_modules, nthreads=nthreads, force=True
-        )
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-
-
-class Compile(_sdist):
-    def __filter_files(self, files):
-        for _files in extensions_dict.values():
-            for file in _files:
-                if file in files:
-                    files.remove(file)
-        return files
-
-    def make_release_tree(self, base_dir, files):
-        if use_cython:
-            files = self.__filter_files(files)
-        _sdist.make_release_tree(self, base_dir, files)
-
-    def run(self):
-        return _sdist.run(self)
-
-
-cmdclass = {
-    'install': PostInstallCommand,
-    'compile': Compile,
-    'build_ext': _build_ext
-}
-
-if has_sphinx:
-    cmdclass['build_sphinx'] = BuildDoc
-
-setup(
+make_setup(
     name='polemarch',
-    packages=find_packages(),
-    ext_modules=ext_modules,
-    include_package_data=True,
+    ext_modules_list=ext_list,
     scripts=['polemarchctl'],
     install_requires=[
-        "django>=1.11,<=2.0",
-    ] + REQUIRES,
+    ] +
+    load_requirements('requirements.txt', os.getcwd()) +
+    load_requirements('requirements-doc.txt', os.getcwd()),
+    extras_require={
+        'test': load_requirements('requirements-test.txt', os.getcwd()),
+    },
     dependency_links=[
-    ] + REQUIRES_git,
-    cmdclass=cmdclass,
+    ] + load_requirements('requirements-git.txt', os.getcwd()),
+    project_urls={
+        "Issue Tracker": "https://gitlab.com/vstconsulting/polemarch/issues",
+        "Documentation": "http://polemarch.readthedocs.io/",
+        "Source Code": "https://gitlab.com/vstconsulting/polemarch",
+        "Releases": "https://github.com/vstconsulting/polemarch/releases",
+    },
 )
