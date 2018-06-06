e38626a020704cd694c7fb36be31ca882685d860
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-06-06 12:31:50 +1000

    Update vstutils to 1.0.11
    Add `ask_user` functions to `ServiceCommand`.
    Fix compilation of package.

--- polemarch/main/management/base.py
+++ polemarch/main/management/base.py
@@ -1,46 +1,47 @@
 # pylint: disable=no-member,invalid-name,broad-except
 from __future__ import absolute_import, unicode_literals
-import logging
-import sys
-
-from django.core.management.base import CommandError as CommandErrorBase
-from django.conf import settings
 import celery
 import ansible
-from vstutils.utils import exception_with_traceback
-from vstutils.management.commands._base import BaseCommand
-
-logger = logging.getLogger("polemarch")
+from django.utils.six.moves import input
+from vstutils.management.commands._base import BaseCommand as _BaseCommand
 
 
-class CommandError(CommandErrorBase):
-    pass
+class BaseCommand(_BaseCommand):
+
+    def _get_versions(self):
+        versions = super(BaseCommand, self)._get_versions()
+        versions['ansible'] = ansible.__version__
+        versions['celery'] = celery.__version__
+        return versions
 
 
-class ServiceCommand(BaseCommand):
-    requires_system_checks = False
-    keep_base_opts = False
-    stdout, stderr = sys.stdout, sys.stderr
+class ServiceCommand(BaseCommand):  # nocv
+    '''
+    Command class for service utils.
+    '''
+    interactive = False
 
     def add_arguments(self, parser):
         super(ServiceCommand, self).add_arguments(parser)
-        parser.add_argument(
-            '-l', '--log-level',
-            action='store',
-            dest='log-level',
-            default=False,
-            type=str,
-            help='Set logs level [debug|warning|error|critical]')
+        if self.interactive:
+            parser.add_argument(
+                '--noinput', '--no-input',
+                action='store_false', dest='interactive', default=True,
+                help="Do NOT prompt the user for input of any kind.",
+            )
 
-    @exception_with_traceback()
     def handle(self, *args, **options):
-        LOG_LEVEL = settings.LOG_LEVEL
-        if options.get('log-level', False):
-            LOG_LEVEL = options.get('log-level', LOG_LEVEL)
-        logger.setLevel(LOG_LEVEL.upper())
-        self.LOG_LEVEL = LOG_LEVEL.upper()
+        super(ServiceCommand, self).handle(*args, **options)
+        self.interactive_mode = options.pop('interactive', False)
 
-    def get_version(self):
-        versions = super(ServiceCommand, self).get_version()
-        vstr = (u' Celery={r.__version__} Ansible={a.__version__}')
-        return versions + vstr.format(r=celery, a=ansible)
+    def ask_user(self, message, default=None):
+        if getattr(self, 'interactive_mode', False):
+            return input(message)
+        return default
+
+    def ask_user_bool(self, message, default=True):
+        reply = self.ask_user(message, 'yes' if default else 'no').lower()
+        if reply in ['y', 'yes']:
+            return True
+        elif reply in ['n', 'no']:
+            return False
--- setup.py
+++ setup.py
@@ -1,19 +1,117 @@
+# Compilation block
+########################################################################################
 import os
 import sys
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+from setuptools.command.sdist import sdist as _sdist
 try:
-    from vstcompile import load_requirements, make_setup, find_packages
-    has_vstcompile = True
+    from Cython.Build import cythonize
 except ImportError:
-    has_vstcompile = False
-    from setuptools import setup as make_setup, find_packages
+    has_cython = False
+else:
+    has_cython = True
 
-    def load_requirements(file_name, folder=os.getcwd()):
-        with open(os.path.join(folder, file_name)) as req_file:
-            return req_file.read().strip().split('\n')
+try:
+    from sphinx.setup_command import BuildDoc
+    import sphinx  # noqa: F401
+    has_sphinx = True
+except ImportError:
+    has_sphinx = False
+
+
+def get_discription(file_path='README.rst', folder=os.getcwd()):
+    with open("{}/{}".format(folder, file_path)) as readme:
+        return readme.read()
+
+
+def load_requirements(file_name, folder=os.getcwd()):
+    with open(os.path.join(folder, file_name)) as req_file:
+        return req_file.read().strip().split('\n')
+
+
+def get_file_ext(ext):
+    file_types = [".py", ".pyx", ".c"] if has_cython else [".c", ".py"]
+    for ftype in file_types:
+        fname = ext.replace(".", "/") + ftype
+        if os.path.exists(fname):
+            return fname
+    return None
+
+
+def make_extensions(extensions_list):
+    if not isinstance(extensions_list, list):
+        raise Exception("Extension list should be `list`.")
+
+    extensions_dict = {}
+    for ext in extensions_list:
+        files = []
+        module_name = ext
+        if isinstance(ext, (list, tuple)):
+            module_name = ext[0]
+            for file_module in ext[1]:
+                file_name = get_file_ext(file_module)
+                files += [file_name] if file_name else []
+        else:
+            file_name = get_file_ext(ext)
+            files += [file_name] if file_name else []
+        if files:
+            extensions_dict[module_name] = files
+
+    ext_modules = list(Extension(m, f) for m, f in extensions_dict.items())
+    ext_count = len(ext_modules)
+    nthreads = ext_count if ext_count < 10 else 10
+
+    if has_cython and 'compile' in sys.argv:
+        return cythonize(ext_modules, nthreads=nthreads, force=True), extensions_dict
+    return ext_modules, extensions_dict
+
+
+class _Compile(_sdist):
+    extensions_dict = dict()
+
+    def __filter_files(self, files):
+        for _files in self.extensions_dict.values():
+            for file in _files:
+                if file in files:
+                    files.remove(file)
+        return files
+
+    def make_release_tree(self, base_dir, files):
+        if has_cython:
+            files = self.__filter_files(files)
+        _sdist.make_release_tree(self, base_dir, files)
+
+    def run(self):
+        return _sdist.run(self)
+
+
+def get_compile_command(extensions_dict=None):
+    extensions_dict = extensions_dict or dict()
+    compile_class = _Compile
+    compile_class.extensions_dict = extensions_dict
+    return compile_class
+
+
+def make_setup(**opts):
+    if 'packages' not in opts:
+        opts['packages'] = find_packages()
+    ext_mod, ext_mod_dict = make_extensions(opts.pop('ext_modules_list', list()))
+    opts['ext_modules'] = opts.get('ext_modules', list()) + ext_mod
+    cmdclass = opts.get('cmdclass', dict())
+    if 'compile' not in cmdclass:
+        cmdclass.update({"compile": get_compile_command(ext_mod_dict)})
+    if has_sphinx and 'build_sphinx' not in cmdclass:
+        cmdclass['build_sphinx'] = BuildDoc
+    opts['cmdclass'] = cmdclass
+    setup(**opts)
+
+########################################################################################
+# end block
 
 
 ext_list = [
@@ -50,6 +148,7 @@ if 'develop' in sys.argv:
 
 kwargs = dict(
     name='polemarch',
+    ext_modules_list=ext_list,
     include_package_data=True,
     install_requires=[
         'vstcompile[doc]',
@@ -71,8 +170,5 @@ kwargs = dict(
         'console_scripts': ['polemarchctl=polemarch:cmd_execution']
     },
 )
-
-if has_vstcompile:
-    kwargs['ext_modules_list'] = ext_list
 
 make_setup(**kwargs)
