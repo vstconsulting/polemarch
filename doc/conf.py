0578a1b417fb7f32d9f0dd35e38c99e996ffef7e
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-06-09 12:38:10 +1000

    Fix compile_docs

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
+import vstutils.wsgi
 import polemarch
 
 # -- General configuration ------------------------------------------------
@@ -99,7 +100,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '../polemarch/static']
+html_static_path = ['_static', '../polemarch/static', vstutils.__path__[0]+'/static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
