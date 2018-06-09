4c9a9cecff85d745fb52216cd28fe3cace8d3273
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-06-09 12:53:49 +1000

    Fix compile_docs

--- doc/conf.py
+++ doc/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-import vstutils.wsgi
+import vstutils
 import polemarch
 
 # -- General configuration ------------------------------------------------
