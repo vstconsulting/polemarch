12db293e0f9c4bfdb2fc8e7622f885a5d0df7cba
Roman <akhmadullin_roman@mail.ru>
2018-06-26 15:26:16 +1000

    Design of collapsed sidebar on Documentation page was refreshed.
    polemarch/ce#55
    
    [ci skip]

--- doc/polemarch-sphinx-theme/static/docs-sidebar.js
+++ doc/polemarch-sphinx-theme/static/docs-sidebar.js
@@ -1,23 +1,19 @@
 $( document ).ready(function() {
-    console.log( "Document is ready!" );
 
-    var headings = $('.toctree-l1');
+    var headings = $('.sidebar-menu .toctree-l1');
 
     for(var i=0; i<headings.length; i++)
     {
-        console.log(headings[i].children[0].innerText);
         var t = headings[i].children[0].innerText;
         $($('.toctree-l1')[i].children[0]).empty();
         $($('.toctree-l1')[i].children[0]).prepend('<i class="fa fa-book doc-sidebar-icon"></i>');
         $($('.toctree-l1')[i].children[0]).append('<span class="docs-heading-wrapper"><span class="docs-heading">' + t + '</span></span>');
-
     }
 
-
-    $(".toctree-l1 .reference.internal").mouseenter(function () {
+    $(".sidebar-menu .toctree-l1 .reference.internal").mouseenter(function () {
         var thisEl = this;
         setTimeout(function () {
-            var docsHeadings = $(".docs-heading-wrapper");
+            var docsHeadings = $(".sidebar-menu .docs-heading-wrapper");
             var bool = false;
             for(var i=0; i<docsHeadings.length; i++)
             {
@@ -29,22 +25,32 @@ $( document ).ready(function() {
 
             if(bool==false)
             {
-                $(".docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
+                $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
                 $(thisEl).children('span').addClass('docs-heading-wrapper-visible');
             }
         }, 200);
 
         $(".content-wrapper").hover(function ()
         {
-            $(".docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
+            $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
         })
 
         $(".navbar").hover(function ()
         {
-            $(".docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
+            $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
         })
     })
 
+    var guiSettings = JSON.parse(window.localStorage.getItem('guiLocalSettings'));
 
-    console.log(headings);
+    if(guiSettings != null)
+    {
+        if(guiSettings.hideMenu !== undefined)
+        {
+            if(guiSettings.hideMenu)
+            {
+                $('body').addClass('sidebar-collapse');
+            }
+        }
+    }
 });
