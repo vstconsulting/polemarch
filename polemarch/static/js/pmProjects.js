c0917fa68778469cf978d2e88661e44f4bab849d
Roman <akhmadullin_roman@mail.ru>
2018-10-23 11:35:11 +1000

    Autocomplete for 'group' field in execute_module was fixed.
    polemarch/ce#128
    
    Functions 'initFields', 'beforeRenderAsPage' and 'beforeRenderAsNewPage' were added.
    
    vst/vst-utils#91
    
    [ci skip]

--- polemarch/static/js/pmProjects.js
+++ polemarch/static/js/pmProjects.js
@@ -1,9 +1,63 @@
- tabSignal.connect("openapi.paths",  function(){
-     let paths = window.guiSchema.path;
-     let exec_module_fields = paths['/project/{pk}/execute_module/'].schema['exec'].fields;
-     let exec_playbook_fields = paths['/project/{pk}/execute_playbook/'].schema['exec'].fields;
-     exec_module_fields['inventory'].type = 'string';
-     exec_module_fields['inventory'].format = 'hybrid_autocomplete';
-     exec_playbook_fields['inventory'].type = 'string';
-     exec_playbook_fields['inventory'].format = 'hybrid_autocomplete';
- });
+function execute_module_group_callback(fieldObj, newValue)
+{
+    let obj = {
+        type: 'autocomplete'
+    }
+
+    if(!isNaN(Number(newValue.value)))
+    {
+        let list_obj = []
+        let new_value = newValue.value
+        let inventory_path = '/inventory/{inventory_id}'
+
+        list_obj.push(projPath + inventory_path + '/all_groups/')
+        list_obj.push(projPath + inventory_path + '/all_hosts/')
+
+        let additional_props = {
+            api_inventory_id: new_value
+        }
+
+        obj.override_opt = {
+            dynamic_properties:{
+                list_obj: list_obj,
+                value_field:'id',
+                view_field:'name',
+                url_vars: additional_props
+            }
+        }
+    }
+
+    return obj;
+}
+
+tabSignal.connect("openapi.schema.definition.AnsibleModule", function(obj) {
+    let properties = obj.definition.properties;
+
+    properties.inventory = {
+        name: 'inventory',
+        title: 'Inventory',
+        type: 'string',
+        format: 'hybrid_autocomplete',
+        dynamic_properties: {
+            list_obj:projPath + "/inventory/",
+            value_field:'id',
+            view_field:'name',
+        },
+        __func__custom_getValue: 'inventory_hybrid_autocomplete_getValue',
+
+    }
+    properties.group = {
+        name: 'group',
+        title: 'Group',
+        type: 'string',
+        format: 'dynamic',
+        default: 'all',
+        parent_field: ['inventory'],
+        dynamic_properties: {
+            __func__callback: 'execute_module_group_callback',
+            value_field:'id',
+            view_field:'name',
+            list_obj: []
+        }
+    }
+})
