20db02034f6c39c8922ba0b761aed422735de9aa
Roman <akhmadullin_roman@mail.ru>
2018-11-15 17:23:12 +1000

    Smart parsing of data field on module detail page was added. 'ansible_json' was improved.
    polemarch/ce#163
    
    'text_paragraph' field was created. 'wrapper_style' block was added to guiElements.base template.
    Function allPropertiesIsObjects() was created.
    
    vst/vst-utils#125
    
    [ci skip]

--- polemarch/static/js/pmProjects.js
+++ polemarch/static/js/pmProjects.js
@@ -167,38 +167,38 @@ gui_project = {
         let formData = {
             title:"Deploy",
             form:{
-                    'inventory' : {
-                        title:'inventory',
-                        required:true,
-                        format:'hybrid_autocomplete',
-                        dynamic_properties:{
-                            list_obj: "/project/{pk}/inventory/",
-                            value_field: "id",
-                            view_field: "name",
-                        }
-                    },
-                    user:{
-                        title:'User',
-                        description: "connect as this user (default=None)",
-                        format:'string',
-                        type: "string",
-                    },
-                    key_file: {
-                        title:'Key file',
-                        description: "use this file to authenticate the connection",
-                        format:'secretfile',
-                        type: "string",
-                        dynamic_properties:{
-                            list_obj: "/project/{pk}/inventory/",
-                            value_field: "id",
-                            view_field: "name",
-                        }
-                    },
-                    extra_vars: {
-                        title:"Execute parametrs",
-                        format:'form',
-                        form:extra_fields
+                'inventory' : {
+                    title:'inventory',
+                    required:true,
+                    format:'hybrid_autocomplete',
+                    dynamic_properties:{
+                        list_obj: "/project/{pk}/inventory/",
+                        value_field: "id",
+                        view_field: "name",
                     }
+                },
+                user:{
+                    title:'User',
+                    description: "connect as this user (default=None)",
+                    format:'string',
+                    type: "string",
+                },
+                key_file: {
+                    title:'Key file',
+                    description: "use this file to authenticate the connection",
+                    format:'secretfile',
+                    type: "string",
+                    dynamic_properties:{
+                        list_obj: "/project/{pk}/inventory/",
+                        value_field: "id",
+                        view_field: "name",
+                    }
+                },
+                extra_vars: {
+                    title:"Execute parametrs",
+                    format:'form',
+                    form:extra_fields
+                }
             }
         }
 
@@ -237,7 +237,7 @@ guiElements.form = function(opt = {}, value, parent_object)
 
                 field = this.prepareFieldOptions(field)
                 let type = getFieldType(field)
- 
+
                 realElements[i] = new guiElements[type]($.extend(true, {}, field), field.value);
             }
         }
@@ -287,49 +287,130 @@ guiElements.form = function(opt = {}, value, parent_object)
 
 guiElements.ansible_json = function(opt = {}, value)
 {
-    /*
-     * This field is only for 1-level json-objects.
-     */
-
     this.name = 'ansible_json'
     guiElements.base.apply(this, arguments)
 
     this.realElements = {};
+
+    this.sortValue = function (value)
+    {
+        let sorted_value = {};
+        let ordered_props = ['short_description', 'description', 'module', 'version_added', 'required', 'requirements', 'extends_documentation_fragment', 'options', 'notes', 'author'];
+
+        // adding to sorted_value fields from ordered_props in correct order
+        for(let i in ordered_props)
+        {
+            let field = ordered_props[i];
+            if(value[field] !== undefined)
+            {
+                sorted_value[field] = value[field];
+            }
+        }
+
+        // adding others field, that are not from ordered_props
+        for(let field in value)
+        {
+            if($.inArray(field, ordered_props) == -1)
+            {
+                sorted_value[field] = value[field];
+            }
+        }
+
+        return sorted_value;
+    }
+
+    /**
+     * Function sets correct types to fields of ansible_json value
+     * @param value(object) - ansible_json value
+     * @param realElements(object) - fields of ansible_json value
+     */
+    this.setTypesOfValueField = function(value, realElements)
+    {
+        let fields_types = {
+            short_description: 'text_paragraph',
+            description: 'text_paragraph',
+            notes: 'text_paragraph',
+        }
+
+        let options_field_title = "Options:";
+        let options_child_fields_subtitle = "Option: ";
+
+        for (let field in value)
+        {
+            let options = {
+                readOnly: opt.readOnly || false,
+                title: capitalizeString(field.replace(/_/g, " ")),
+            }
+
+            let type = 'string';
+            if (fields_types[field] && opt.title != options_field_title)
+            {
+                type = fields_types[field];
+
+                if (opt.title && opt.title.search(options_child_fields_subtitle) != -1)
+                {
+                    options.hide_title = true;
+                }
+            }
+            else if (typeof value[field] == "string" && value[field].length > 50)
+            {
+                type = 'textarea';
+            }
+            else if (typeof value[field] == 'boolean')
+            {
+                type = 'boolean';
+            }
+            else if (typeof value[field] == 'object')
+            {
+                if (Array.isArray(value[field]))
+                {
+                    type = 'textarea';
+                }
+                else if (value[field] === null)
+                {
+                    type = 'null';
+                }
+                else
+                {
+                    type = 'ansible_json';
+                    if (allPropertiesIsObjects(value[field]))
+                    {
+                        options.divider = true;
+                        options.title += ":";
+                    }
+                    else
+                    {
+                        if (opt.title == options_field_title)
+                        {
+                            options.title = options_child_fields_subtitle + field;
+                        }
+                    }
+                }
+            }
+
+            if ((typeof value[field] == "string" && value[field] == "") || (Array.isArray(value[field]) && value[field].length == 0)) {
+                type = 'hidden';
+            }
+
+            realElements[field] = new guiElements[type]($.extend({}, options), value[field]);
+        }
+
+        return realElements;
+    }
+
     this.setValue = function(value)
     {
         this.value = value
         let realElements = {};
         if(value)
         {
-            for(let field in value)
-            {
-                let options = {
-                    readOnly: opt.readOnly || false,
-                    title: field,
-                }
-
-                let type = 'string';
-
-                if (Array.isArray(value[field]))
-                {
-                	type = 'textarea';
-                }
-                else if(typeof value[field] == 'boolean')
-                {
-                    type = 'boolean';
-                }
-                else if(typeof value[field] == 'object')
-                {
-                	type = 'ansible_json';
-                }
-
-                realElements[field] = new guiElements[type]($.extend({}, options), value[field]);
-            }
+            value = this.sortValue(value);
+            realElements = this.setTypesOfValueField(value, realElements);
         }
 
-        this.realElements = realElements
-
+        this.realElements = realElements;
     }
+
     this.setValue(value)
 
     this.insertTestValue = function(value)
@@ -341,7 +422,6 @@ guiElements.ansible_json = function(opt = {}, value)
     this.getValue = function()
     {
         let valueObj = {};
-        debugger;
         for(let element_name in this.realElements)
         {
             let element = this.realElements[element_name];
@@ -354,7 +434,6 @@ guiElements.ansible_json = function(opt = {}, value)
     this.getValidValue = function()
     {
         let valueObj = {};
-        debugger;
         for(let element_name in this.realElements)
         {
             let element = this.realElements[element_name];
@@ -370,5 +449,6 @@ tabSignal.connect("openapi.schema.schema", function(obj)
     if (obj.path == '/project/{pk}/module/{module_id}/')
     {
         obj.value.fields.data.format = 'ansible_json'
+        obj.value.fields.data.hide_title = true;
     }
 })
