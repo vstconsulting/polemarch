91cf6021ae191d5782806120550a43ee35ea6277
Roman <akhmadullin_roman@mail.ru>
2018-11-27 15:04:35 +1000

    Fix profile tests.
    polemarch/ce#174
    
    [ci skip]

--- polemarch/static/js/tests/pmUsers.js
+++ polemarch/static/js/tests/pmUsers.js
@@ -4,15 +4,10 @@ window.qunitTestsArray['guiPaths.profile/settings'] = {
     {
         let path = "profile/settings"
         guiTests.openPage(path)
-        guiTests.hasElement(0, ".btn_save", path)
-        guiTests.hasEditButton(true)
+        guiTests.hasElement(1, ".btn_save", path)
+        guiTests.hasEditButton(false)
         guiTests.hasAddButton(0, path)
 
-
-        path = "profile/settings/edit"
-        guiTests.openPage(path)
-
-        guiTests.hasElement(1, ".btn_save", path)
         guiTests.hasElement(1, ".gui-field-chartLineSettings", path)
 
         guiTests.hasAddButton(0, path)
