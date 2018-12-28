23e7103c72d8de4418c5e2408928078250261ef2
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-12-28 10:33:26 +1000

    Update Community view description

--- polemarch/static/js/pmCommunity.js
+++ polemarch/static/js/pmCommunity.js
@@ -2,3 +2,8 @@ tabSignal.connect('openapi.schema', function(obj){
     let path = obj.schema.path['/community_template/{pk}/use_it/'];
     path.schema.exec.fields.project_id.hidden = true;
 });
+
+tabSignal.connect("openapi.schema.definition.OneProjectTemplate", function (obj) {
+    let props = obj.definition.properties;
+    props['description'].format = 'text_paragraph';
+});
