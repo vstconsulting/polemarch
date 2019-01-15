91597ac96f19f06e5d6eb2b0c43c4980f47abb6d
Sergey Klyuykov <onegreyonewhite@mail.ru>
2019-01-15 15:59:57 +1000

    Build docs for openapi with examples and schemas

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,11 @@ except ImportError:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.httpdomain', 'sphinxcontrib.openapi']
+extensions = [
+    'sphinxcontrib.httpdomain',
+    'sphinxcontrib.openapi',
+    'vstutils.api.doc_generator'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
