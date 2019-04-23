afb38b49f64d0eb05efa68f5d4c937c4890b1943
Sergey Klyuykov <onegreyonewhite@mail.ru>
2019-04-23 13:50:00 +1000

    Update autorelease script

--- autorelease.sh
+++ autorelease.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CURRENT_VERSION=$(cat polemarch/__init__.py | grep __version__ | cut -d "=" -f2 | grep -oE "[0-9\.](\.dev[0-9]{1,2}){0,1}"| tr -d '\n')
+CURRENT_VERSION=$(python setup.py --version | tr -d '\n')
 TAG=$(git tag -l $CURRENT_VERSION)
 
 if [ -z "${TAG}" ]; then
