6cf4136aba5be0ce2965be1e7eb96c374ae7ca78
Sergey Klyuykov <onegreyonewhite@mail.ru>
2019-12-19 12:25:36 +1000

    Fix building https://gitlab.com/vstconsulting/polemarch/-/jobs/384059147

--- polemarch/__init__.py
+++ polemarch/__init__.py
@@ -31,6 +31,6 @@ default_settings = {
     "VST_ROOT_URLCONF": os.getenv("VST_ROOT_URLCONF", 'vstutils.urls'),
 }
 
-__version__ = "1.6.1.post1"
+__version__ = "1.6.1.post2"
 
 prepare_environment(**default_settings)
