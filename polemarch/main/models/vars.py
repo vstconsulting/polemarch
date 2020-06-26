57a9a0439762b6bbc01e12c14dc9bde09e930c25
Kirill Bychkov <kirill970528@yandex.ru>
2020-06-26 08:20:44 +0000

    [WIP] Create tests for Imported Inventories.
    Think about compare logic for all groups and hosts.

--- polemarch/main/models/vars.py
+++ polemarch/main/models/vars.py
@@ -133,12 +133,12 @@ class AbstractModel(ACLModel):
         encr = "[~~ENCRYPTED~~]"
         encrypted_vars = {k: v for k, v in variables.items() if v == encr}
         other_vars = {k: v for k, v in variables.items() if v != encr}
-        self.variables.exclude(key__in=encrypted_vars.keys()).delete()
+        self.variables.cleared().exclude(key__in=list(encrypted_vars.keys()) + list(other_vars.keys())).delete()
         for key, value in other_vars.items():
             self.variables.create(key=key, value=value)
 
     def get_vars(self) -> Union[OrderedDict, Dict]:
-        qs = self.variables.all().sort_by_key().values_list('key', 'value')
+        qs = self.variables.cleared().sort_by_key().values_list('key', 'value')
         return reduce(update_boolean, self.BOOLEAN_VARS, OrderedDict(qs))
 
     def get_vars_prefixed(self, prefix: Text):
